b72fa4061828ceccea49af0fe8ec61c4ba00f428
JonMrowczynski <jonmrowczynski@gmail.com>
2026-01-31 21:16:59 -0500

    Cleaned up Strings.java

--- Python vs Java/2 - Variables/Strings.java
+++ Python vs Java/2 - Variables/Strings.java
@@ -8,40 +8,69 @@
 public class Strings {
 
     public static void main(String[] args) {
-        // Strings literals require double quotes.
+        stringConcatenation();
+        integerStringFormatting();
+        floatStringFormatting();
+        stringMethodsExamples();
+        stringComparisonExamples();
+        textBlockExample();
+    }
+
+    /**
+     * Shows how to concatenate Strings. Notice that String literals have to be created with double quotes. Single quotes are
+     * used for characters in Java.
+     */
+    private static void stringConcatenation() {
+        System.out.println("String Concatenation Example:");
         var firstHalf = "Can concate";
         var secondHalf = "nate Strings!";
         System.out.println(firstHalf + secondHalf);
         System.out.println();
+    }
 
+    /**
+     * Offers a few examples displaying some of the most common ways to format integers when printing. Notice that you can group
+     * digits in code with '_' rather than a ','.
+     */
+    private static void integerStringFormatting() {
+        System.out.println("Integer String Formatting Examples:");
         var myInt = 15; // Change me!
-        
-        System.out.println(String.format("Printing an int: %d", myInt));
-        System.out.println(String.format("Printing the same int in hex: %x", myInt));
-        System.out.println(String.format("Printing a large int with digit groupings: %,d", 1_000_000_000));
-        // Notice that you can group digits in code with '_' rather than a ','.
+        System.out.printf("Printing an int: %d\n", myInt);
+        System.out.printf("Printing the same int in hex: %x\n", myInt);
+        System.out.printf("Printing a large int with digit groupings: %,d\n", 1_000_000_000);
         System.out.println();
+    }
 
+    /**
+     * Offers a few examples displaying some of the most common ways to format floating-point numbers when printing. 
+     */
+    private static void floatStringFormatting() {
+        System.out.println("Float String Formatting Examples:");
         var myFloat = Math.PI; // Change me!
-
-        System.out.println(String.format("Printing a float: %f", myFloat));
-        System.out.println(String.format("Printing the same float to two decimal places: %.2f", myFloat));
+        System.out.printf("Printing a float: %f\n", myFloat);
+        System.out.printf("Printing the same float to two decimal places: %.2f\n", myFloat);
         System.out.println();
+    }
 
-        printMenuComplex();
-        System.out.println("\n");
-        printMenuSimple();
+    /**
+     * Strings are immutable, so if you perform any operation on a String, a new one is created.
+     */
+    private static void stringMethodsExamples() {
+        System.out.println("String Methods Example");
+        var str = "Hello World!"; // Change me with care! (You probably will need to change the below method calls)
+        System.out.println(str.substring(1, 3));
+        System.out.println(str.charAt(7));
+        System.out.println(str.contains("Wo"));
+        System.out.println(str.indexOf("lo"));
         System.out.println();
+    }
 
-        // Strings are immutable, so if you perform any operation on a String, a new one has to be created.
+    /**
+     * Shows some intricacies of Java String comparisons.
+     */
+    private static void stringComparisonExamples() {
         var str1 = "Hello World!";
-        System.out.println(str1.substring(1, 3));
-        System.out.println(str1.charAt(7));
-        System.out.println(str1.contains("Wo"));
-        System.out.println(str1.indexOf("lo"));
-        System.out.println();
-
-        var str2 = "Hello World"; // Even though this is a new String variable, it points to the same String in the String Constant Pool.
+        var str2 = "Hello World!"; // Even though this is a new String variable, it points to the same String in the String Constant Pool!
         System.out.println("str1 == \"Hello World!\"? -> " + (str1 == str2));
         System.out.println("str1.equals(str2)? -> " + (str1.equals(str2)));
         System.out.println("str1 == new String(\"Hello World!\")? -> " + (str1 == new String("Hello World!")));
@@ -50,9 +79,19 @@ public class Strings {
     }
 
     /**
+     * Illustrates how to use String text blocks to more simply get Strings with complicated formats.
+     */
+    private static void textBlockExample() {
+        printMenuComplex();
+        System.out.println("\n");
+        printMenuSimple();
+        System.out.println();
+    }
+
+    /**
      * A basic simple menu that could be cleaned up as shown below.
      */
-    public static void printMenuComplex() {
+    private static void printMenuComplex() {
         System.out.println("Welcome to the program! Enter:\n");
         System.out.println("\t1. For Option 1.");
         System.out.println("\t2. For Options 2.");
@@ -66,7 +105,7 @@ public class Strings {
      * significantly more complicated formatted outputs. The same formatting ideas presented above can be applied to text blocks
      * as well since text blocks are also just Strings!
      */
-    public static void printMenuSimple() {
+    private static void printMenuSimple() {
         System.out.print("""
                 Welcome to the program! Enter:
 
