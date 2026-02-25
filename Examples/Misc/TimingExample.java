391dd6210ff998ae5370f18004c78c99f9eecd9f
JonMrowczynski <jonmrowczynski@gmail.com>
2026-02-24 21:36:51 -0500

    Updated for loop in TimingExample.java

--- Examples/Misc/TimingExample.java
+++ Examples/Misc/TimingExample.java
@@ -4,13 +4,10 @@ public class TimingExample {
         final var ti = System.nanoTime();
         // Do some process...
         var sum = 0L;
-        for (var i = 0; i < 1_000_000_000; ++i) { 
-            for (var j = 0; j < 10; ++j) { sum += j; }
-            sum += i;
-        }
+        for (var i = 1; i < Integer.MAX_VALUE; ++i) { sum += i; }
         final var tf = System.nanoTime();
         final var deltaT = (tf - ti) / 1_000_000_000.0;
-        System.out.println("It took " + deltaT + "s to run the process!");
+        System.out.println("It took " + deltaT + "s to sum all the integers, which yields: " + sum);
     }
 
 }
