1c704924a476cce1fb85443884c0709433f7f17d
JonMrowczynski <jonmrowczynski@gmail.com>
2026-03-22 23:10:55 -0400

    Added Person object in bad package

--- Examples/11 - Inheritence/BasicInheritence/src/bad/Person.java
+++ Examples/11 - Inheritence/BasicInheritence/src/bad/Person.java
@@ -1,3 +1,41 @@
 package bad;
 
-public class Person { }
+/**
+ * @author Jon Mrowczynski
+ */
+public class Person {
+    // Instance Fields
+    private final String firstName;
+    private final String lastName; 
+    private int age;
+
+    // Constructors
+    public Person(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+    public Person(String firstName, String lastName, int age) {
+        this(firstName, lastName); // Calls the other constructor with the given arguments!
+        this.age = age;
+    }
+
+    // Getters
+    public String getFirstName() { return firstName; }
+    public String getLastName() { return lastName; }
+    public int getAge() { return age; }
+
+    // Setters
+    public void setAge(int newAge) throws IllegalArgumentException { 
+        if (newAge < 0) { throw new IllegalArgumentException("Age cannot be negative"); }
+        age = newAge; 
+    }
+    
+    // Other Methods
+    public String getName() { return firstName + " " + lastName; }
+    @Override public String toString() {
+        return """
+                First Name: %s
+                Last Name: %s
+                Age: %d""".formatted(firstName, lastName, age);
+    }
+}
