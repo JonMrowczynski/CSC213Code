422477212b1587e820965b5be0e73a5aff851a6e
JonMrowczynski <jonmrowczynski@gmail.com>
2026-03-31 10:14:21 -0400

    Made Square final.

--- Examples/11 - Inheritance/AbstractClasses/src/shapes/impl1/Square.java
+++ Examples/11 - Inheritance/AbstractClasses/src/shapes/impl1/Square.java
@@ -3,7 +3,7 @@ package shapes.impl1;
 /**
  * @author Jon Mrowczynski
  */
-public class Square extends Rectangle {
+public final class Square extends Rectangle {
     public Square(String name, double sideLength) {
         super(name, sideLength, sideLength); // Call's the superclass's (Rectangle's) constructor.
     }
