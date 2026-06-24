6dd4169e2aac5ff0af86850769ff59e94a6cffa3
JonMrowczynski <jonmrowczynski@gmail.com>
2026-06-24 17:37:03 -0400

    Updated BasicInheritance to Java 25

--- Examples/11 - Inheritance/BasicInheritance/src/BasicInheritance.java
+++ Examples/11 - Inheritance/BasicInheritance/src/BasicInheritance.java
@@ -1,19 +1,20 @@
-public class BasicInheritance {
-	public static void main(String[] args) {
-		var firstName = "Alice";
-		var lastName = "Smith";
-		var age = 42;
-		var school = "Canisius";
-		
-		var teacherBadImpl = new bad.Teacher(firstName, lastName, age, school);
-		System.out.println(teacherBadImpl);
-		
-		var teacherGoodImpl = new good.Teacher(firstName, lastName, age, school);
-		System.out.println(teacherGoodImpl);
-		
-		System.out.println("\nTeachers equal?: -> " + teacherGoodImpl.equals(teacherBadImpl));
-		
-		System.out.println("Bad Teacher getName() -> " + teacherBadImpl.getName());
-		System.out.println("Good Teacher getName() -> " + teacherGoodImpl.getName());
-	}
+/**
+ * @author Jon Mrowczynski
+ */
+void main() {
+	var firstName = "Alice";
+	var lastName = "Smith";
+	var age = 42;
+	var school = "Canisius";
+	
+	var teacherBadImpl = new bad.Teacher(firstName, lastName, age, school);
+	IO.println(teacherBadImpl);
+	
+	var teacherGoodImpl = new good.Teacher(firstName, lastName, age, school);
+	IO.println(teacherGoodImpl);
+	
+	IO.println("\nTeachers equal?: -> " + teacherGoodImpl.equals(teacherBadImpl));
+	
+	IO.println("Bad Teacher getName() -> " + teacherBadImpl.getName());
+	IO.println("Good Teacher getName() -> " + teacherGoodImpl.getName());
 }
