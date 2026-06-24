887fcebdf2269244fc0e5d46287136051728cafb
JonMrowczynski <jonmrowczynski@gmail.com>
2026-06-24 17:13:02 -0400

    Updated Exceptions to Java 25

--- Examples/13 - Exceptions/src/ExceptionsExamples.java
+++ Examples/13 - Exceptions/src/ExceptionsExamples.java
@@ -1,96 +1,80 @@
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.util.InputMismatchException;
-import java.util.NoSuchElementException;
-import java.util.Optional;
-import java.util.Scanner;
-
 /**
  * @author Jon Mrowczynski
  */
-public class ExceptionsExamples {
-	
-	private static final String FILE_NAME = "writtenData.txt";
-	
-	public static void main(String[] args) {
-		uncheckedException();
-		//		int[] myInts = {1, 2, 3};
-		//		final var optInts = add3(myInts);
-		//		optInts.ifPresent(arr -> System.out.println(Arrays.toString(arr)));
+final String FILE_NAME = "writtenData.txt";
+
+Optional<int[]> add3(int[] myInts) {
+	if (myInts == null) { return Optional.empty(); }
+	for (var i = 0; i < myInts.length; ++i) { myInts[i] += 3; }
+	return Optional.of(myInts);
+}
+
+// Can either catch or throw a checkedException
+void checkedException() /* throws IOException */ {
+	try (var fos = new FileOutputStream("MyFile.txt")) {
+		IO.println("In try.");
 	}
-	
-	/**
-	 * Basically any RuntimeException is unchecked.
-	 */
-	private static void uncheckedException() {
-		final var scanner = new Scanner(System.in);
-		try {
-			final var myInt = scanner.nextInt();
-			System.out.println("After input.");
-		}
-		catch (InputMismatchException e) {
-			System.out.println("do a thing here.");
-		}
-		catch (NoSuchElementException e) {
-			System.out.println("do another thing here");
-		}
+	catch (IOException e) { IO.println("In catch."); }
+}
+
+void writingWithExplicitClose(int exceptionNum) {
+	PrintWriter writer = null;
+	try {
+		IO.println("In try block:");
+		if (exceptionNum == 0) { return; }
+		var fos = new FileOutputStream(FILE_NAME); // Should technically close this
+		if (exceptionNum == 1) { throw new FileNotFoundException("Exception 1 thrown!"); }
+		IO.println("\tMade FileOutputStream.");
+		writer = new PrintWriter(fos);
+		writer.println("Hello World!");
+		if (exceptionNum == 2) { throw new FileNotFoundException("Exception 2 thrown!"); }
+		writer.close();
 	}
-	
-	private static Optional<int[]> add3(int[] myInts) {
-		if (myInts == null) { return Optional.empty(); }
-		for (var i = 0; i < myInts.length; ++i) { myInts[i] += 3; }
-		return Optional.of(myInts);
-	}
-	
-	// Can either catch or throw a checkedException
-	private static void checkedException() /* throws IOException */ {
-		try (var fos = new FileOutputStream("MyFile.txt")) {
-			System.out.println("In try.");
-		}
-		catch (IOException e) { System.out.println("In catch."); }
-	}
-	
-	private static void writingWithExplicitClose(int exceptionNum) {
-		PrintWriter writer = null;
-		try {
-			System.out.println("In try block:");
-			if (exceptionNum == 0) { return; }
-			var fos = new FileOutputStream(FILE_NAME); // Should technically close this
-			if (exceptionNum == 1) { throw new FileNotFoundException("Exception 1 thrown!"); }
-			System.out.println("\tMade FileOutputStream.");
-			writer = new PrintWriter(fos);
-			writer.println("Hello World!");
-			if (exceptionNum == 2) { throw new FileNotFoundException("Exception 2 thrown!"); }
+	catch (FileNotFoundException e) {
+		IO.println("In catch block due to -> " + e);
+		// Sometimes you just have a really bad day.
+		//			throw e; // If this gets thrown, we don't execute code after the finally block.
+	} finally {
+		IO.println("In finally block:");
+		if (writer != null) {
 			writer.close();
+			IO.println("\tClosed writer.");
 		}
-		catch (FileNotFoundException e) {
-			System.out.println("In catch block due to -> " + e);
-			// Sometimes you just have a really bad day.
-			//			throw e; // If this gets thrown, we don't execute code after the finally block.
-		} finally {
-			System.out.println("In finally block:");
-			if (writer != null) {
-				writer.close();
-				System.out.println("\tClosed writer.");
-			}
-			else { System.out.println("\tWriter was never instantiated, so can't close."); }
-		}
-		System.out.println("I am outside:");
+		else { IO.println("\tWriter was never instantiated, so can't close."); }
 	}
-	
-	private static void writingWithTryWithResourcesWithExceptions(int exceptionNum) {
-		try (var writer = new PrintWriter(new FileOutputStream(FILE_NAME))) {
-			System.out.println("In try block:");
-			if (exceptionNum == 1) { throw new FileNotFoundException("Exception 1 thrown!"); }
-			writer.println("Hello World!");
-			if (exceptionNum == 2) { throw new FileNotFoundException("Exception 2 thrown!"); }
-		}
-		catch (IOException e) {
-			System.out.println("In catch block:");
-			// throw e;
-		} // Can still add a finally block, but don't need.
-		System.out.println("I am outside:");
+	IO.println("I am outside:");
+}
+
+void writingWithTryWithResourcesWithExceptions(int exceptionNum) {
+	try (var writer = new PrintWriter(new FileOutputStream(FILE_NAME))) {
+		IO.println("In try block:");
+		if (exceptionNum == 1) { throw new FileNotFoundException("Exception 1 thrown!"); }
+		writer.println("Hello World!");
+		if (exceptionNum == 2) { throw new FileNotFoundException("Exception 2 thrown!"); }
 	}
+	catch (IOException e) {
+		IO.println("In catch block:");
+		// throw e;
+	} // Can still add a finally block, but don't need.
+	IO.println("I am outside:");
+}
+
+void main() {
+	uncheckedException();
+	//		int[] myInts = {1, 2, 3};
+	//		final var optInts = add3(myInts);
+	//		optInts.ifPresent(arr -> IO.println(Arrays.toString(arr)));
+}
+
+/**
+ * Basically any RuntimeException is unchecked.
+ */
+void uncheckedException() {
+	final var scanner = new Scanner(System.in);
+	try {
+		final var myInt = scanner.nextInt();
+		IO.println("After input.");
+	}
+	catch (InputMismatchException e) { IO.println("do a thing here."); }
+	catch (NoSuchElementException e) { IO.println("do another thing here"); }
 }
