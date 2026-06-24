6ed8c53425c1b78e157a591c859f082abba0aad0
JonMrowczynski <jonmrowczynski@gmail.com>
2026-06-24 17:24:55 -0400

    Updated 14 - Generics to Java 25 + cleaned up code

--- Examples/14 - Generics/src/GenericExamples.java
+++ Examples/14 - Generics/src/GenericExamples.java
@@ -1,56 +1,55 @@
-import java.util.List;
+// All these methods below can be replaced with a singular generic method!
+
+double averageShorts(List<Short> list) {
+	var sum = 0.0;
+	for (var item : list) { sum += item; }
+	return sum / list.size();
+}
+
+double averageInts(List<Integer> list) {
+	var sum = 0.0;
+	for (var item : list) { sum += item; }
+	return sum / list.size();
+}
+
+double averageDoubles(List<Double> list) {
+	var sum = 0.0;
+	for (var item : list) { sum += item; }
+	return sum / list.size();
+}
 
 /**
  * @author Jon Mrowczynski
  */
-public class GenericExamples {
+void main() {
+	var shortsList = List.of((short) 1, (short) 2, (short) 3);
+	var intsList = List.of(1, 2, 3);
+	var doublesList = List.of(1.0, 2.0, 3.0);
 	
-	public static void main(String[] args) {
-		List<Short> shortsList = List.of((short) 1, (short) 2, (short) 3);
-		List<Integer> intsList = List.of(1, 2, 3);
-		List<Double> doublesList = List.of(1.0, 2.0, 3.0);
-		
-		//		var shortsAverage = average(shortsList);
-		//		var intsAverage = average(intsList);
-		//		var doublesAverage = average(doublesList);
-		
-		var shortsAverage = average(shortsList);
-		var intsAverage = average(intsList);
-		var doublesAverage = average(doublesList);
-		
-		System.out.printf("""
-				                  Short average: %f
-				                  Int average: %f
-				                  Double average: %f%n""", shortsAverage, intsAverage, doublesAverage);
-		
-		// Show generic example.
-		
-		// Show self-referential
-	}
+	//		var shortsAverage = averageShorts(shortsList);
+	//		var intsAverage = averageInts(intsList);
+	//		var doublesAverage = averageDoubles(doublesList);
 	
-	private static <T extends Number> double average(List<T> list) {
-		var sum = 0.0;
-		for (T item : list) { sum += item.doubleValue(); }
-		return sum / list.size();
-	}
+	var shortsAverage = average(shortsList);
+	var intsAverage = average(intsList);
+	var doublesAverage = average(doublesList);
 	
-	// All the methods below can be replaced with a singular generic method!
+	IO.println("""
+			           Short average: %f
+			           Int average: %f
+			           Double average: %f""".formatted(shortsAverage, intsAverage, doublesAverage));
 	
-	private static double averageShorts(List<Short> list) {
-		var sum = 0.0;
-		for (var item : list) { sum += item; }
-		return sum / list.size();
-	}
+	// Show generic example.
 	
-	private static double averageInts(List<Integer> list) {
-		var sum = 0.0;
-		for (var item : list) { sum += item; }
-		return sum / list.size();
-	}
-	
-	private static double averageDoubles(List<Double> list) {
-		var sum = 0.0;
-		for (var item : list) { sum += item; }
-		return sum / list.size();
-	}
+	// Show self-referential
+}
+
+<T extends Number> double average(List<T> list) {
+	var sum = 0.0;
+	for (T item : list) { sum += item.doubleValue(); }
+	return sum / list.size();
+}
+
+<T extends Number> double averageWithStreams(List<T> list) {
+	return list.stream().mapToDouble(Number::doubleValue).average().orElse(Double.NaN);
 }
